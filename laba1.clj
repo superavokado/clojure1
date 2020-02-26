51c91b07f46cee349c02cb4d23e09529305dda66
Kristina <58972561+superavokado@users.noreply.github.com>
2020-02-26 18:29:20 +0300

    short version

--- laba1.clj
+++ laba1.clj
@@ -1,23 +1,14 @@
 (ns FunctionalProgrammingLab1.laba1)
 
-(defn dfs [current finish ways visited]
-  (do
-    (if-not (contains? visited current)
-      (if (= current finish)
-        (do
-          (lazy-seq (conj visited current))
-          )
-        (for [possible (ways current)]
-          (do
-            (lazy-seq (dfs possible finish ways (conj visited current)))
-            )
-          )
+(defn bfs [current finish ways visited]
+  (if-not (contains? visited current)
+    (if (= current finish)
+      (lazy-seq (conj visited current))
+      (for [possible (ways current)]
+        (lazy-seq (bfs possible finish ways (conj visited current)))
         )
       )
     )
   )
 
-(let [res (dfs 0 4 [[1 2] [0 2] [0 1 3] [2 4] [3]] [])]
-  (println "result")
-  (println res)
-  )
+(println (bfs 0 4 [[1 2] [0 2] [0 1 3] [2 4] [3]] []))
