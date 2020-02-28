2064b0da2c9f353ad9b06754dec8dfd02d5d0eec
Kristina <58972561+superavokado@users.noreply.github.com>
2020-02-28 13:45:15 +0300

    Add files via upload

--- laba1.clj
+++ laba1.clj
@@ -4,11 +4,11 @@
   (if-not (contains? visited current)
     (if (= current finish)
       (lazy-seq (conj visited current))
-      (for [possible (ways current)]
+      (for [possible (get ways current)]
         (lazy-seq (bfs possible finish ways (conj visited current)))
         )
       )
     )
   )
 
-(println (bfs 0 4 [[1 2] [0 2] [0 1 3] [2 4] [3]] []))
+(println (bfs 0 4 (hash-map 0 [1 2], 1 [0 2], 2 [0 1 3], 3 [2 4], 4 [3]) []))
