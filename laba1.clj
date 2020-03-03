3b927dd35b3e927bea9ec8f74e57152c94222f8e
Kristina <58972561+superavokado@users.noreply.github.com>
2020-03-03 10:28:20 +0300

    Change all

--- laba1.clj
+++ laba1.clj
@@ -1,14 +1,24 @@
-(ns FunctionalProgrammingLab1.laba1)
 
-(defn bfs [current finish ways visited]
-  (if-not (contains? visited current)
-    (if (= current finish)
-      (lazy-seq (conj visited current))
-      (for [possible (get ways current)]
-        (lazy-seq (bfs possible finish ways (conj visited current)))
+(def graph [{:value 15 :name "Kris" :nei [1 4]} {:value 220 :name "Andrew" :nei [0 2]}
+            {:value 320 :name "Alex" :nei [1 3]} {:value 44 :name "Ira" :nei [2 5]}
+            {:value 50 :name "Kirill" :nei [0 5]} {:value 630 :name "Ura" :nei [3 4]}])
+
+(defn search [graph start key_v value]
+  (loop [curr nil queue (apply list (:nei (get graph start))) visited #{}]
+    (if (empty? queue)
+      nil
+      (if (and (not (nil? curr)) (not (contains? curr visited)))
+        (if (= (key_v curr) value)
+          curr
+          (recur (get graph (first queue))
+                 (apply list (distinct (into (pop queue) (:nei curr))))
+                 (cons visited curr))
+          )
+        (recur (get graph (first queue)) (pop queue) visited)
         )
       )
     )
   )
 
-(println (bfs 0 4 (hash-map 0 [1 2], 1 [0 2], 2 [0 1 3], 3 [2 4], 4 [3]) []))
+(search graph 0 :name "Alex")
+(search graph 0 :value 630)
